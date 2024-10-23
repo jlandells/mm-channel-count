2834527d23e41d0a7613f38118707c68b3efd13c
John Landells <john.landells@me.com>
2024-10-23 14:06:59 +0100

    Initial creation

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # mm-channel-count
-Retrieves a total count of channels per team for a given user
+
+`mm-channel-count` is a utility designed to generate a count of all channels a specified user is a member of, both on a per-team basis and as a total across all teams, within a Mattermost instance. It is intended for Mattermost sysadmins and integrates with the Mattermost API to retrieve and process this information.
+
+## Getting Started
+
+To use `mm-channel-count`, download the appropriate executable for your platform from the [Releases](https://github.com/jlandells/mm-channel-count/releases) page
+Make sure to place the downloaded executable in your `PATH` or reference it directly when running commands.
+
+## Usage
+
+### Basic Command
+
+To run `mm-channel-count`, you must specify the Mattermost server URL and API token, along with the username for which the channel count should be generated:
+
+```bash
+mm-channel-count -url=https://mattermost.example.com -token=your_api_token -username=sample.user
+```
+
+### Command Line Parameters
+
+| **Command Line** | **Environment** | **Notes** |
+| --- | --- | --- |
+| `-url` | `MM_URL` | ***Required**. The Mattermost host that will receive the API requests. |
+| `-scheme` | `MM_SCHEME` | `http` / `https`. Defaults to `http`. |
+| `-port` | `MM_PORT` | The port used to reach the Mattermost instance. Defaults to `8065`. |
+| `-token` | `MM_TOKEN` | ***Required**. The API token used to access Mattermost. The user **must** have sysadmin rights. |
+| `-username` |  | ***Required**. The username for which the channel count should be generated. |
+| `-debug` | `MM_DEBUG` | Executes the application in debug mode, providing additional output. |
+| `-version` |  | Prints the current version and exits. |
+| `-help` |  | Displays usage instructions and exits. |
+
+### Examples
+
+**Counting channels for a specific user:**
+
+```bash
+mm-channel-count -url=https://mattermost.example.com -token=your_api_token -username=sample.user
+```
+
+**Using environment variables:**
+
+```bash
+export MM_URL=https://mattermost.example.com
+export MM_TOKEN=your_api_token
+mm-channel-count -username=sample.user
+```
+
+**Enabling debug mode:**
+
+```bash
+mm-channel-count -url=https://mattermost.example.com -token=your_api_token -username=sample.user -debug=true
+```
+
+In all examples, command-line parameters will override corresponding environment variables.
+
+## Contributing
+
+We welcome contributions from the community! Whether it's a bug report, a feature suggestion, or a pull request, your input is valuable to us. Please feel free to contribute in the following ways:
+- **Issues and Pull Requests**: For specific questions, issues, or suggestions for improvements, open an issue or a pull request in this repository.
+- **Mattermost Community**: Join the discussion in the [Integrations and Apps](https://community.mattermost.com/core/channels/integrations) channel on the Mattermost Community server.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contact
+
+For questions, feedback, or contributions regarding this project, please use the following methods:
+- **Issues and Pull Requests**: For specific questions, issues, or suggestions for improvements, feel free to open an issue or a pull request in this repository.
+- **Mattermost Community**: Join us in the Mattermost Community server, where we discuss all things related to extending Mattermost. You can find me in the channel [Integrations and Apps](https://community.mattermost.com/core/channels/integrations).
+- **Social Media**: Follow and message me on Twitter, where I'm [@jlandells](https://twitter.com/jlandells).
+
---
+++ main.go
@@ -0,0 +1,333 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mattermost/mattermost/server/public/model"
+)
+
+var Version = "development" // Default value - overwritten during bild process
+
+var debugMode bool = false
+
+// LogLevel is used to refer to the type of message that will be written using the logging code.
+type LogLevel string
+
+type mmConnection struct {
+	mmURL    string
+	mmPort   string
+	mmScheme string
+	mmToken  string
+}
+
+const (
+	debugLevel   LogLevel = "DEBUG"
+	infoLevel    LogLevel = "INFO"
+	warningLevel LogLevel = "WARNING"
+	errorLevel   LogLevel = "ERROR"
+)
+
+const (
+	defaultPort   = "8065"
+	defaultScheme = "http"
+	pageSize      = 60
+	maxErrors     = 3
+)
+
+type Team struct {
+	Name         string
+	ID           string
+	ChannelCount int
+}
+
+type User struct {
+	ID        string
+	Username  string
+	Email     string
+	FirstName string
+	LastName  string
+	NickName  string
+	Teams     []Team
+}
+
+// Logging functions
+
+// LogMessage logs a formatted message to stdout or stderr
+func LogMessage(level LogLevel, message string) {
+	if level == errorLevel {
+		log.SetOutput(os.Stderr)
+	} else {
+		log.SetOutput(os.Stdout)
+	}
+	log.SetFlags(log.Ldate | log.Ltime)
+	log.Printf("[%s] %s\n", level, message)
+}
+
+// DebugPrint allows us to add debug messages into our code, which are only printed if we're running in debug more.
+// Note that the command line parameter '-debug' can be used to enable this at runtime.
+func DebugPrint(message string) {
+	if debugMode {
+		LogMessage(debugLevel, message)
+	}
+}
+
+// getEnvWithDefaults allows us to retrieve Environment variables, and to return either the current value or a supplied default
+func getEnvWithDefault(key string, defaultValue interface{}) interface{} {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+func GetUserIDFromUsername(mmClient model.Client4, username string) (*User, error) {
+	DebugPrint("Getting user ID for user: " + username)
+
+	ctx := context.Background()
+	etag := ""
+
+	user, response, err := mmClient.GetUserByUsername(ctx, username, etag)
+
+	if err != nil {
+		LogMessage(errorLevel, "Failed to retrieve user: "+err.Error())
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		LogMessage(errorLevel, "Function call to GetUserByUsername returned bad HTTP response")
+		return nil, errors.New("bad HTTP response")
+	}
+
+	mmUser := &User{
+		ID:        user.Id,
+		Username:  username,
+		Email:     user.Email,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		NickName:  user.Nickname,
+	}
+
+	return mmUser, nil
+}
+
+func GetChannelCountForTeam(mmClient model.Client4, teamID string, userID string) (int, error) {
+	DebugPrint("Getting channel count for team ID: " + teamID)
+
+	channelCount := -1
+	ctx := context.Background()
+	etag := ""
+
+	channels, response, err := mmClient.GetChannelsForTeamForUser(ctx, teamID, userID, false, etag)
+
+	if err != nil {
+		LogMessage(errorLevel, "Failed to retrieve channels: "+err.Error())
+		return channelCount, err
+	}
+	if response.StatusCode != 200 {
+		LogMessage(errorLevel, "Function call to GetChannelsForTeamForUser returned bad HTTP response")
+		return channelCount, errors.New("bad HTTP response")
+	}
+
+	channelCount = len(channels)
+
+	return channelCount, nil
+}
+
+func GetTeamsForUser(mmClient model.Client4, userID string) ([]Team, error) {
+
+	DebugPrint("Getting teams for user ID: " + userID)
+
+	ctx := context.Background()
+	etag := ""
+
+	teams, response, err := mmClient.GetTeamsForUser(ctx, userID, etag)
+	if err != nil {
+		LogMessage(errorLevel, "Failed to retrieve teams: "+err.Error())
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		LogMessage(errorLevel, "Function call to GetTeamsForUser returned bad HTTP response")
+		return nil, errors.New("bad HTTP response")
+	}
+
+	var teamsList []Team
+
+	for _, mmTeam := range teams {
+		team := Team{
+			Name: mmTeam.DisplayName,
+			ID:   mmTeam.Id,
+		}
+
+		teamsList = append(teamsList, team)
+	}
+
+	return teamsList, nil
+}
+
+func PrintSummary(user User) {
+
+	totalChannelCount := 0
+
+	fmt.Printf("\n\n")
+	fmt.Printf("Summary\n")
+	fmt.Printf("=======\n\n")
+	fmt.Printf("Username: %s\n", user.Username)
+	fmt.Printf("Email:    %s\n", user.Email)
+	fmt.Printf("Name:     %s %s\n", user.FirstName, user.LastName)
+	fmt.Printf("Nickname: %s\n\n", user.NickName)
+	fmt.Printf("Teams\n")
+	fmt.Printf("=====\n\n")
+
+	// Figure out the longest team name to assist with formatting
+	maxTeamNameLength := 0
+	for _, team := range user.Teams {
+		if len(team.Name) > maxTeamNameLength {
+			maxTeamNameLength = len(team.Name)
+		}
+	}
+
+	// Add some padding
+	maxTeamNameLength += 2
+
+	// Now we can print the Teams portion
+	for _, team := range user.Teams {
+		fmt.Printf("%-*s : %d\n", maxTeamNameLength, team.Name, team.ChannelCount)
+		totalChannelCount += team.ChannelCount
+	}
+
+	fmt.Printf("\nTotal channel count : %d\n\n", totalChannelCount)
+}
+
+func main() {
+
+	// Parse Command Line
+	DebugPrint("Parsing command line")
+
+	var MattermostURL string
+	var MattermostPort string
+	var MattermostScheme string
+	var MattermostToken string
+	var MattermostUser string
+	var DebugFlag bool
+	var VersionFlag bool
+
+	flag.StringVar(&MattermostURL, "url", "", "The URL of the Mattermost instance (without the HTTP scheme)")
+	flag.StringVar(&MattermostPort, "port", "", "The TCP port used by Mattermost. [Default: "+defaultPort+"]")
+	flag.StringVar(&MattermostScheme, "scheme", "", "The HTTP scheme to be used (http/https). [Default: "+defaultScheme+"]")
+	flag.StringVar(&MattermostToken, "token", "", "The auth token used to connect to Mattermost")
+	flag.StringVar(&MattermostUser, "user", "", "The username of the Mattermost user")
+	flag.BoolVar(&DebugFlag, "debug", false, "Enable debug output")
+	flag.BoolVar(&VersionFlag, "version", false, "Show version information and exit")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintln(flag.CommandLine.Output(), "This utility is used to find how many channels a users is member of.")
+		fmt.Fprintln(flag.CommandLine.Output(), "Options:")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if VersionFlag {
+		fmt.Printf("mm-channel-count - Version: %s\n\n", Version)
+		os.Exit(0)
+	}
+
+	// If information not supplied on the command line, check whether it's available as an envrionment variable
+	if MattermostURL == "" {
+		MattermostURL = getEnvWithDefault("MM_URL", "").(string)
+	}
+	if MattermostPort == "" {
+		MattermostPort = getEnvWithDefault("MM_PORT", defaultPort).(string)
+	}
+	if MattermostScheme == "" {
+		MattermostScheme = getEnvWithDefault("MM_SCHEME", defaultScheme).(string)
+	}
+	if MattermostToken == "" {
+		MattermostToken = getEnvWithDefault("MM_TOKEN", "").(string)
+	}
+	if !DebugFlag {
+		DebugFlag = getEnvWithDefault("MM_DEBUG", debugMode).(bool)
+	}
+
+	DebugMessage := fmt.Sprintf("Parameters: \n  MattermostURL=%s\n  MattermostPort=%s\n  MattermostScheme=%s\n  MattermostToken=%s\n  User=%s\n",
+		MattermostURL,
+		MattermostPort,
+		MattermostScheme,
+		MattermostToken,
+		MattermostUser)
+	DebugPrint(DebugMessage)
+
+	// Validate required parameters
+	DebugPrint("Validating parameters")
+	var cliErrors bool = false
+	if MattermostURL == "" {
+		LogMessage(errorLevel, "The Mattermost URL must be supplied either on the command line of vie the MM_URL environment variable")
+		cliErrors = true
+	}
+	if MattermostScheme == "" {
+		LogMessage(errorLevel, "The Mattermost HTTP scheme must be supplied either on the command line of vie the MM_SCHEME environment variable")
+		cliErrors = true
+	}
+	if MattermostToken == "" {
+		LogMessage(errorLevel, "The Mattermost auth token must be supplied either on the command line of vie the MM_TOKEN environment variable")
+		cliErrors = true
+	}
+	if MattermostUser == "" {
+		LogMessage(errorLevel, "A Mattermost username is required to use this utility.")
+		cliErrors = true
+	}
+
+	if cliErrors {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	debugMode = DebugFlag
+
+	// Prepare the Mattermost connection
+	mattermostConenction := mmConnection{
+		mmURL:    MattermostURL,
+		mmPort:   MattermostPort,
+		mmScheme: MattermostScheme,
+		mmToken:  MattermostToken,
+	}
+
+	mmTarget := fmt.Sprintf("%s://%s:%s", mattermostConenction.mmScheme, mattermostConenction.mmURL, mattermostConenction.mmPort)
+
+	DebugPrint("Full target for Mattermost: " + mmTarget)
+	mmClient := model.NewAPIv4Client(mmTarget)
+	mmClient.SetToken(mattermostConenction.mmToken)
+	DebugPrint("Connected to Mattermost")
+
+	LogMessage(infoLevel, "Processing started - Version: "+Version)
+
+	// Get the ID (and other information) of the user
+	user, err := GetUserIDFromUsername(*mmClient, MattermostUser)
+	if err != nil {
+		LogMessage(errorLevel, "Failed to retrieve user from Mattermost")
+		os.Exit(10)
+	}
+
+	// Get the teams that this user is a member of
+	teams, err := GetTeamsForUser(*mmClient, user.ID)
+	if err != nil {
+		LogMessage(errorLevel, "Failed to retrieve teams from Mattermost")
+		os.Exit(11)
+	}
+
+	user.Teams = teams
+
+	for i := range teams {
+		teams[i].ChannelCount, err = GetChannelCountForTeam(*mmClient, teams[i].ID, user.ID)
+		if err != nil {
+			LogMessage(warningLevel, "Failed to get channel count for team "+teams[i].Name)
+		}
+	}
+
+	PrintSummary(*user)
+}
